30c5371f6821ee106ef8b39fadb05d0ca7fbbc21
johnwargo <john@johnwargo.com>
2026-01-06 17:47:58 -0500

    working away

--- eleventy-plugin-post-links.js
+++ eleventy-plugin-post-links.js
@@ -1,1 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cli_logger_1 = __importDefault(require("cli-logger"));
+const APP_NAME = 'Eleventy-Plugin-Post-Links';
+var conf = { console: true, level: cli_logger_1.default.INFO };
+conf.prefix = function (record) {
+    return `[${APP_NAME}]`;
+};
+var log = (0, cli_logger_1.default)(conf);
+module.exports = function (eleventyConfig, options = {}) {
+    eleventyConfig.addShortcode("postLinks", function () {
+        return "<< Post Links Section >>";
+    });
+};
---
+++ ts-src/eleventy-plugin-post-links.ts
@@ -0,0 +1,35 @@
+/***********************************************
+ * Eleventy Plugin Post Links
+ * 
+ * By John M. Wargo
+ * https://johnwargo.com
+ ***********************************************/
+
+//@ts-ignore
+import logger from 'cli-logger';
+
+type ModuleOptions = {
+  debugMode?: boolean,
+  useSection?: boolean,
+  sectionTitle?: string
+}
+
+const APP_NAME = 'Eleventy-Plugin-Post-Links';
+
+// configure the logger
+var conf: any = { console: true, level: logger.INFO };
+conf.prefix = function (record: any) {
+  return `[${APP_NAME}]`;
+}
+var log = logger(conf);
+
+module.exports = function (eleventyConfig: any, options: ModuleOptions = {}) {
+  eleventyConfig.addShortcode("postLinks", function () {
+    
+     const debugMode = options.debugMode || false;
+    log.level(debugMode ? log.DEBUG : log.INFO);
+    log.debug('Debug mode enabled\n');
+
+    return "<< Post Links Section >>";
+  });
+}
