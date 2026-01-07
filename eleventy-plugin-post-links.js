bce1d3f83669f4db880dc285dc6625c11e02c59e
johnwargo <john@johnwargo.com>
2026-01-07 07:30:06 -0500

    working on link parsing

--- eleventy-plugin-post-links.js
+++ eleventy-plugin-post-links.js
@@ -1,9 +1,28 @@
+import markdownLinkExtractor from 'markdown-link-extractor';
+const defaultConfig = {
+    'collapsible': false,
+    'contentScope': 'PostLinksScope',
+    'externalLinksOnly': false,
+    'minimumLinks': 0,
+    'openInNewTab': true,
+    'orderedList': false,
+    'sectionTitle': 'Links',
+    'debugMode': true
+};
 const APP_NAME = 'Eleventy-Plugin-Post-Links';
 export default function (eleventyConfig, options = {}) {
     eleventyConfig.addShortcode("postLinks", function () {
+        options = { ...defaultConfig, ...options };
+        if (options.debugMode) {
+            console.log('Options');
+            console.table(options);
+        }
         if (options.collapsible && !options.sectionTitle) {
             return `[${APP_NAME}] When using 'collapsible' option, you must also provide a 'sectionTitle' option.`;
         }
+        console.dir(this.page);
+        const links = markdownLinkExtractor(this.page.rawInput);
+        console.dir(links);
         return "<< Post Links Section >>";
     });
 }
--- ts-src/eleventy-plugin-post-links.ts
+++ ts-src/eleventy-plugin-post-links.ts
@@ -4,24 +4,58 @@
  * By John M. Wargo
  * https://johnwargo.com
  ***********************************************/
+//@ts-ignore
+import { UserConfig } from '@11ty/eleventy';
+//@ts-ignore
+import markdownLinkExtractor from 'markdown-link-extractor';
 
 type ModuleOptions = {
   collapsible?: boolean,
+  contentScope?: string,
   externalLinksOnly?: boolean,
+  minimumLinks?: number,
   openInNewTab?: boolean,
   orderedList?: boolean,
   sectionTitle?: string,
   debugMode?: boolean
 }
 
+const defaultConfig: ModuleOptions = {
+  'collapsible': false,
+  'contentScope': 'PostLinksScope',
+  'externalLinksOnly': false,
+  'minimumLinks': 0,
+  'openInNewTab': true,
+  'orderedList': false,
+  'sectionTitle': 'Links',
+  'debugMode': true
+}
+
 const APP_NAME = 'Eleventy-Plugin-Post-Links';
 
-export default function (eleventyConfig: any, options: ModuleOptions = {}) {
+export default function (eleventyConfig: UserConfig, options: ModuleOptions = {}) {
+
   eleventyConfig.addShortcode("postLinks", function () {
+
+    // Merge the user options with the default config
+    options = { ...defaultConfig, ...options };
+    if (options.debugMode) {
+      console.log('Options');
+      console.table(options);
+    }
 
     if (options.collapsible && !options.sectionTitle) {
       return `[${APP_NAME}] When using 'collapsible' option, you must also provide a 'sectionTitle' option.`;
     }
+
+    //@ts-ignore
+    // console.log(this.page.title);
+    //@ts-ignore
+    // console.dir(this.page);
+
+    //@ts-ignore
+    const links = markdownLinkExtractor(this.page.rawInput);
+    console.dir(links);
 
     return "<< Post Links Section >>";
   });
